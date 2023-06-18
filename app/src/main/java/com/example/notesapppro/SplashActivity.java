e504ea5b96a71f84f84fad39ef66844008db1ec0
Lucute2001 <lch.ryu2001@gmail.com>
2023-06-19 00:08:19 +0700

    3.Firebase Connect | Create Account Authentication | Notes app with Firebase Series | 2022

--- app/src/main/java/com/example/notesapppro/SplashActivity.java
+++ app/src/main/java/com/example/notesapppro/SplashActivity.java
@@ -19,6 +19,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(new Intent(SplashActivity.this, CreateAccountActivity.class));
+                finish();
             }
         }, 1000);
     }
