3f752aef9113e8bdfd28a431496a4fdc8dbf25d9
Lucute2001 <lch.ryu2001@gmail.com>
2023-06-19 01:16:13 +0700

    6.Save Notes In Firebase Firestore Database | Notes app with Firebase Series | 2022

--- app/src/main/java/com/example/notesapppro/NoteDetailsActivity.java
+++ app/src/main/java/com/example/notesapppro/NoteDetailsActivity.java
@@ -1,10 +1,16 @@
 package com.example.notesapppro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
 
 public class NoteDetailsActivity extends AppCompatActivity {
     EditText titleEditText,contentEditText;
@@ -29,5 +35,27 @@ public class NoteDetailsActivity extends AppCompatActivity {
             titleEditText.setError("Title is required");
             return;
         }
+        Note note = new Note();
+        note.setTitle(noteTitle);
+        note.setContent(noteContent);
+        note.setTimestamp(Timestamp.now());
+        saveNoteToFirebase(note);
+    }
+
+    private void saveNoteToFirebase(Note note) {
+        DocumentReference documentReference;
+        documentReference = Utility.getCollectionReferenceForNotes().document();
+        documentReference.set(note).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Utility.ShowToast(NoteDetailsActivity.this, "Node added successfully");
+                    finish();
+                }
+                else {
+                    Utility.ShowToast(NoteDetailsActivity.this, "Failed while adding note");
+                }
+            }
+        });
     }
 }
